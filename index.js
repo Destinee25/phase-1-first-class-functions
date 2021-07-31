36e563445269a29d3a21c68e339d47605c4a86c8
Destinee Robinson <Destineerobinson08@gmail.com>
2021-07-30 22:09:59 -0400

    Phase one assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+function receivesAFunction(spy){
+  let chai = ['chai']
+    console.log(spy)
+    spy(chai)
+  }
+receivesAFunction(chai)
+
+let msg = 'returns a function'
+function returnsANamedFunction(fn){
+  let fn
+}
